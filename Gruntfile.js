a1b9886e5d968db67feccb8041183e818d5fab8e
Thiago de Arruda <tpadilha84@gmail.com>
2013-10-10 10:20:37 -0300

    Configure saucelabs with grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,28 @@
+// browsers to test in saucelabs
+var browsers = [{
+  browserName: "firefox",
+  version: "19",
+  platform: "XP"
+}, {
+  browserName: "chrome",
+  platform: "XP"
+}, {
+  browserName: "chrome",
+  platform: "linux"
+}, {
+  browserName: "internet explorer",
+  platform: "WIN8",
+  version: "10"
+}, {
+  browserName: "internet explorer",
+  platform: "VISTA",
+  version: "9"
+}, {
+  browserName: "opera",
+  platform: "Windows 2008",
+  version: "12"
+}];
+
 
 module.exports = function(grunt) {
   grunt.initConfig({
@@ -13,11 +38,7 @@ module.exports = function(grunt) {
       },
       test: {
         files: [{
-          src: [
-            'node_modules/grunt-mocha-debug/node_modulesrun-mocha/index.js',
-            'test/**/*.js'
-          ],
-          dest: 'build/test-bundle.js'
+          src: 'test/**/*.js', dest: 'build/test-bundle.js'
         }]
       }
     },
@@ -34,8 +55,24 @@ module.exports = function(grunt) {
       },
       browser: {
         options: {
+          listenAddress: '127.0.0.1',
+          listenPort: 9999,
           phantomjs: true,
           src: ['build/test-bundle.js']
+        }
+      }
+    },
+
+    'saucelabs-mocha': {
+      all: {
+        options: {
+          urls: ["http://127.0.0.1:9999"],
+          tunnelTimeout: 5,
+          build: process.env.TRAVIS_JOB_ID,
+          concurrency: 3,
+          browsers: browsers,
+          testname: "mocha tests",
+          tags: ["master"]
         }
       }
     },
@@ -57,9 +94,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('powerbuild');
   grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-saucelabs');
   grunt.registerTask('test', ['newer:exec_jshint', 'powerbuild', 'mocha_debug']);
 
   grunt.registerTask('publish', ['mocha_debug', 'release']);
-  grunt.registerTask('travis', ['test']);
+  grunt.registerTask('travis', ['test', 'saucelabs-mocha']);
   grunt.registerTask('default', ['test', 'watch']);
 };
