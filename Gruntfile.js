007af3e112c2a90c38ba50bb7ed629e59896303d
Thiago de Arruda <tpadilha84@gmail.com>
2013-10-10 07:32:07 -0300

    Replace testling with travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,22 @@ module.exports = function(grunt) {
       all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
     },
 
+    powerbuild: {
+      options: {
+        sourceMap: true,
+        compress: true
+      },
+      test: {
+        files: [{
+          src: [
+            'node_modules/grunt-mocha-debug/node_modulesrun-mocha/index.js',
+            'test/**/*.js'
+          ],
+          dest: 'scripts/test-bundle.js'
+        }]
+      }
+    },
+
     mocha_debug: {
       options: {
         reporter: 'dot',
@@ -15,6 +31,12 @@ module.exports = function(grunt) {
       nodejs: {
         options: {
           src: ['src/**/*.js', 'test/**/*.js']
+        }
+      },
+      browser: {
+        options: {
+          phantomjs: true,
+          src: ['scripts/test-bundle.js']
         }
       }
     },
@@ -33,10 +55,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-exec-jshint');
   grunt.loadNpmTasks('grunt-mocha-debug');
+  grunt.loadNpmTasks('powerbuild');
   grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-release');
-  grunt.registerTask('test', ['newer:exec_jshint', 'mocha_debug']);
+  grunt.registerTask('test', ['newer:exec_jshint', 'powerbuild', 'mocha_debug']);
 
   grunt.registerTask('publish', ['mocha_debug', 'release']);
+  grunt.registerTask('travis', ['exec_jshint', 'mocha_debug']);
   grunt.registerTask('default', ['test', 'watch']);
 };
